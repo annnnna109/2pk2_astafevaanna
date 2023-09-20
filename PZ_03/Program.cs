ecbcd595e5f0bc7545e16296cd07cd765dc2c8d6
annnnna109 <144040921+annnnna109@users.noreply.github.com>
2023-09-20 18:17:51 +0500

    добавлено 4 практическое зедание

--- PZ_03/Program.cs
+++ PZ_03/Program.cs
@@ -4,7 +4,53 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите год:");
+            int year = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите номер месяца (от 1 до 12):");
+            int month = Convert.ToInt32(Console.ReadLine());
+
+            int daysInMonth = GetDaysInMonth(year, month);
+            Console.WriteLine("Количество дней в указанном месяце: " + daysInMonth);
+        }
+
+        static int GetDaysInMonth(int year, int month)
+        {
+            bool isLeapYear = IsLeapYear(year);
+
+            switch (month)
+            {
+                case 2:
+                    return isLeapYear ? 29 : 28;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    return 30;
+                default:
+                    return 31;
+            }
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            if (year % 4 != 0)
+            {
+                return false;
+            }
+            else if (year % 100 != 0)
+            {
+                return true;
+            }
+            else if (year % 400 != 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
         }
     }
-}
+}    
---
+++ Pz_04/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Pz_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Задание 1.");
+            // Задание 1.
+
+            for (int r = 0; r <= 90; r += 5) // Цикл с шагом 5 
+            {
+                Console.WriteLine(r); // Вывод числа
+            }
+
+
+            Console.WriteLine("Задание 2.");
+            // Задание 2.
+
+            char startChar = 'b';  // Начальный символ
+            for (int d = 0; d < 6; d++) // Количество символов
+            {
+                char currentChar = (char)(startChar +d); // Вычисление текущего символа
+                Console.WriteLine(currentChar + ""); // Вывод текущего символа
+            }
+
+
+            Console.WriteLine("Задание 3.");
+            // Задание 3.
+
+            for (int f = 0; f < 5; f++) // Цикл для строк
+            {
+                for (int h = 0;h < 4; h++) //Цикл для символов в строке
+                {
+                    Console.Write("#");  // Вывод символа "#"
+                }
+                Console.WriteLine(); // Переход на новую строку
+            }
+
+
+            Console.WriteLine("Задание 4.");
+            // Задание 4.
+
+            int count = 0; // Переменная для подсчета количества кратных чисел
+
+            for (int k = -200; k <= 200; k++) // Цикл для перебора чисел в диапазоне
+            {
+                if (k % 7 == 0) // Проверка на кратность числа 7
+                {
+                    Console.Write(k + " "); // Вывод кратного числа с пробелом
+                    count++; // Увеличение счетчика
+                }
+            }
+
+            Console.WriteLine("\nКоличество кратных чисел: " + count); // Вывод количества кратных чисел
+
+
+            Console.WriteLine("Задание 5.");
+            // Задание 5.
+
+            int i = 1;
+            int j = 40;
+
+            for (int aa = i, oo = j; j - aa >= 20; aa++, oo--)
+            {
+                Console.WriteLine("i: " + aa + ", j: " + oo);
+            }
+        }
+    }
+
+}
