833cc51c4135248e63a8c61aca74fdb81ec5ea7b
annnnna109 <144040921+annnnna109@users.noreply.github.com>
2023-09-22 17:21:04 +0500

    добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double distance = 10; // Начальный пробег спортсмена
+            int day = 1; // Номер текущего дня
+
+            while (distance < 20) // Проверка пробега спортсмена
+            {
+                distance += distance * 0.1; // Увеличение пробега на 10%
+
+                day++; // Увеличение номера дня
+            }
+
+            Console.WriteLine($"Пробег спортсмена составит не менее 20 километров на {day} день");
+        }
+    
+    
+    }
+}
