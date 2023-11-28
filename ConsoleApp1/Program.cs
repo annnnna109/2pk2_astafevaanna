5ce203bf2b431f64b1da479bcc184275215dbbd4
annnnna109 <144040921+annnnna109@users.noreply.github.com>
2023-11-28 19:14:48 +0500

    добавлена 12 пратика

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char sum1 = 'e';
+            char sum2 = 'n';
+            char result = sum1==sum2 ? sum1++ :++ sum2;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ PZ_12/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+             double GetPunctuationPercentage(string text)
+            {
+                int punctuationCount = 0;
+                int totalChars = text.Length;
+
+                foreach (char c in text)
+                {
+                    if (char.IsPunctuation(c))
+                    {
+                        punctuationCount++;
+                    }
+                }
+
+                double percentage = (double)punctuationCount / totalChars * 100;
+                return percentage;
+            }
+
+            string input = "Это текст с знаками препинания!";
+            double punctuationPercentage = GetPunctuationPercentage(input);
+            Console.WriteLine("Процентное соотношение знаков препинания: {0}%", punctuationPercentage);
+
+        }
+    }
+}
