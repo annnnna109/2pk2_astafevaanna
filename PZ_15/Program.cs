fb72095066b6f5971f09ecd35aa6e7b9c30a562f
annnnna109 <144040921+annnnna109@users.noreply.github.com>
2023-12-05 13:00:46 +0500

    добавлено 15 пз

---
+++ PZ_15/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace PZ_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите полный путь к каталогу: ");
+            string directoryPath = Console.ReadLine();
+
+            // Проверка наличия каталога по указанному пути
+            if (Directory.Exists(directoryPath))
+            {
+                // Получение списка файлов в каталоге
+                string[] files = Directory.GetFiles(directoryPath, "*.exe");
+
+                // Вывод информации о каждом файле в списке
+                foreach (string file in files)
+                {
+                    FileInfo fileInfo = new FileInfo(file);
+                    Console.WriteLine($"Имя файла: {fileInfo.Name}, " +
+                                      $"Время создания: {fileInfo.CreationTime}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Каталог не найден");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
