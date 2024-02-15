e469b29b28faad6548f4d7d4842add5c85ab4cd1
annnnna109 <144040921+annnnna109@users.noreply.github.com>
2024-02-15 12:12:12 +0500

    Добавлена 18 практика

---
+++ PZ_18 Вариант 5/CreditClient.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_18_Вариант_5
+
+{
+    public enum CreditHistoryStatus
+    {
+        High, Medium,
+        Low
+    }
+    internal class CreditClient
+    {
+        private static decimal totalLoanAmount; private static int totalClientCount;
+        private string fio;
+        private CreditHistoryStatus creditHistory; private decimal loanAmount;
+        private decimal interestRate;
+        public string FIO
+        {
+            get { return fio; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Предупреждающее сообщение: ФИО не может быть пустым");
+                    Console.WriteLine("Введите ФИО заново:"); string input = Console.ReadLine();
+                    FIO = input;
+                }
+                else
+                {
+                    fio = value;
+                }
+            }
+        }
+        public CreditHistoryStatus CreditHistory
+        {
+            get { return creditHistory; }
+            set { creditHistory = value; }
+        }
+        public decimal LoanAmount
+        {
+            get { return loanAmount; }
+            set
+            {
+                if (value < 100000 || value > 5000000)
+                {
+                    Console.WriteLine("Предупреждающее сообщение: Сумма кредита должна быть от 100 тыс. до 5 млн.");
+                }
+                else
+                {
+                    loanAmount = value;
+                }
+            }
+        }
+        public decimal InterestRate
+        {
+            get { return interestRate; }
+        }
+        public static decimal TotalLoanAmount
+        {
+            get { return totalLoanAmount; }
+        }
+        public static int TotalClientCount
+        {
+            get { return totalClientCount; }
+        }
+        public CreditClient(string fio, CreditHistoryStatus creditHistory, decimal loanAmount)
+        {
+            FIO = fio; CreditHistory = creditHistory;
+            LoanAmount = loanAmount;
+            CalculateInterestRate(); totalLoanAmount += loanAmount;
+            totalClientCount++;
+        }
+        public void PrintClientInformation()
+        {
+            Console.WriteLine("Информация о клиенте:");
+            Console.WriteLine($"ФИО: {fio}"); Console.WriteLine($"Сумма кредита: {loanAmount}");
+            Console.WriteLine($"Процент кредита: {interestRate}"); Console.WriteLine($"Статус кредитной истории: {creditHistory}");
+        }
+        public void GrantLoan()
+        {
+            if (creditHistory == CreditHistoryStatus.High)
+            {
+                Console.WriteLine("Кредит выдан! Процент кредита составляет 10%.");
+            }
+            else if (creditHistory == CreditHistoryStatus.Medium)
+            {
+                Console.WriteLine("Кредит выдан! Процент кредита составляет 12%.");
+            }
+            else
+            {
+                Console.WriteLine("Кредит не выдан! Отказ в кредите.");
+            }
+        }
+        public static void PrintCreditInformation()
+        {
+            Console.WriteLine("Общая информация о кредитах:"); Console.WriteLine($"Сумма всех выданных кредитов: {totalLoanAmount}");
+            Console.WriteLine($"Количество выданных кредитов: {totalClientCount}");
+            Console.WriteLine($"Остаток суммы на кредиты: {50000000 - totalLoanAmount}");
+        }
+        private void CalculateInterestRate()
+        {
+            if (creditHistory == CreditHistoryStatus.High)
+            {
+                interestRate = 10;
+            }
+            else if (creditHistory == CreditHistoryStatus.Medium)
+            {
+                interestRate = 12;
+            }
+            else
+            {
+                interestRate = 0;
+            }
+        }
+    }
+}
---
+++ PZ_18 Вариант 5/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PZ_18_Вариант_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditClient client1 = new CreditClient("Мухамбетова Алина Сериковна", CreditHistoryStatus.High, 100000); client1.PrintClientInformation();
+            client1.GrantLoan();
+            CreditClient client2 = new CreditClient("Сарбаева Азалия Зинуровна", CreditHistoryStatus.Medium, 3000000); client2.PrintClientInformation();
+            client2.GrantLoan();
+            CreditClient client3 = new CreditClient("Винс Елена Алексеевна", CreditHistoryStatus.Low, 5000000); client3.PrintClientInformation();
+            client3.GrantLoan();
+            CreditClient.PrintCreditInformation();
+        }
+    }
+}
